1d39059081fc10e859e20de37322825416988309
Morc <michaux.cl@gmail.com>
2021-01-13 14:49:32 +0100

    Delete includes directory

--- includes/php/connect-db.php
+++
@@ -1,9 +0,0 @@
-<?php
-	 try
-		{
-			$db = new PDO('mysql:host=localhost;dbname=iplogger;charset=utf8', 'username', 'password');
-		}
-	catch(Exception $e) 
-		{
-			die('Erreur : '.$e->getMessage());
-		}
